034e2f318cc83509f193d7158ddf6e0f1f0ae595
tobey10 <fafowora.oluwatobiloba@academicianhelp.com>
2021-11-11 21:40:26 +0100

    category and ingredient services

---
+++ src/app/model/ingredient.ts
@@ -0,0 +1,8 @@
+import { Category } from "./category";
+
+export class Ingredient{
+  category!: Category;
+  ingreident!: String;
+  calorie!: Number;
+  caloriUnit!: String
+}
--- src/app/services/APIs/apis.ts
+++ src/app/services/APIs/apis.ts
@@ -4,4 +4,8 @@ export class APIS{
   public static authURL = APIS.generalURL +"auth/";
 
   public static userURL = APIS.generalURL + "user/";
+
+  public static categoryURL = APIS.generalURL + "category/";
+
+  public static ingredientURL = APIS.generalURL + "ingredient/";
 }
